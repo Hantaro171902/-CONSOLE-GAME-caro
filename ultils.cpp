73f72e0ffb8dd0c26042e39712be27d52a33634b
Hantaro171902 <chaugiahan001@gmail.com>
2025-07-05 23:46:41 +0700

    update ultils.cpp terminal

--- ultils.cpp
+++ ultils.cpp
@@ -40,8 +40,67 @@ bool kbhit() {
 
 #else
 
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 void gotoXY(int x, int y) {
     printf("\033[%d;%dH", y + 1, x + 1);
 }
 
+void clearScreen() {
+    printf("\033[2J\033[1;1H");
+}
 
+void setTextColor(int color) {
+    // ANSI escape codes for text colors
+    switch (color) {
+        case 30: printf("\033[30m"); break; // Black
+        case 31: printf("\033[31m"); break; // Red
+        case 32: printf("\033[32m"); break; // Green
+        case 33: printf("\033[33m"); break; // Yellow
+        case 34: printf("\033[34m"); break; // Blue
+        case 35: printf("\033[35m"); break; // Magenta
+        case 36: printf("\033[36m"); break; // Cyan
+        case 37: printf("\033[37m"); break; // White
+        default: printf("\033[0m"); break; // Reset to default
+    }
+}
+
+void hideCursor() {
+    printf("\033[?25l"); // Hide cursor
+}
+
+int getch() {
+    struct termios oldt, newt;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO); // Disable canonical mode and echo
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return ch;
+}
+
+bool kbhit() {
+    struct termios oldt, newt;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO); // Disable canonical mode and echo
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    
+    ch = getchar();
+    
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    
+    if(ch != EOF) {
+        ungetc(ch, stdin); // Put the character back into the input buffer
+        return true;
+    }
+    
+    return false;
+}
+
+#endif
