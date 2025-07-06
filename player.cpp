eb56a4f4671f64ae0b9495b9c0b7e0b7b67edb19
Hantaro171902 <chaugiahan001@gmail.com>
2025-07-06 21:41:46 +0700

    add player.cpp

--- player.cpp
+++ player.cpp
@@ -1,8 +1,30 @@
 // player.cpp
 #include "player.h"
+#include <iostream>
 
-Player::Player(CellState type) : playerType(type) {}
+using namespace std;
+
+Player::Player(CellState type, const string& name) 
+: playerType(type), playerName(name) {}
 
 CellState Player::getType() const {
     return playerType;
+}
+
+string Player::getName() const {
+    return playerName;
+}
+
+void Player::setName(const string& newName) {
+    playerName = newName;
+}
+
+void Player::displayInfo() const {
+    cout << "Player: " << playerName << " (";
+    if (playerType == CellState::PLAYER_X) {
+        cout << "X";
+    } else {
+        cout << "O";
+    }
+    cout << ")" << endl;
 }
