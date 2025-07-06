3d4ee0a5d4089717d112378a55e446e3f21c169b
Hantaro171902 <chaugiahan001@gmail.com>
2025-07-06 21:48:42 +0700

    add game_logic.cpp

--- game_logic.cpp
+++ game_logic.cpp
@@ -1,45 +1,66 @@
-#include "game.h"
-#include "ultils.h"
-#include "board.h"
+// game_logic.cpp
+#include "game_logic.h"
+#include "utils.h"
 #include <iostream>
 
-Game::Game() : currentPlayer(CellState::PLAYER_X), isRunning(true) {}
+using namespace std;
+
+Game::Game() : playerX(CellState::PLAYER_X), playerO(CellState::PLAYER_O), isRunning(false) {
+    currentPlayer = &playerX;
+}
+
+void Game::init() {
+    board.reset();
+    currentPlayer = &playerX;
+    isRunning = true;
+}
 
 void Game::run() {
-    board.reset();
     while (isRunning) {
-        board.draw();
-        cout << "Player " << (currentPlayer == CellState::PLAYER_X ? "X" : "O") << "'s turn. " << endl;
-
-        int row, col;
-        cout << "Enter row (0-" << BOARD_SIZE - 1 << "): ";
-        cin >> row;
-        cout << "Enter column (0-" << BOARD_SIZE - 1 << "): ";
-        cin >> col;
-
-        if (!board.placeMove(row, col, currentPlayer)) {
-            cout << "Invalid move. Try again." << endl;
-            continue;
-        }
-
-        if (board.checkWin(row, col, currentPlayer)) {
-            board.draw();
-            // displayWinner();
-            cout << "Player " << (currentPlayer == CellState::PLAYER_X ? "X" : "O") << " wins!" << endl;
-            isRunning = false;
-            break;
-        } 
-
-        switchPlayer();
+        render();
+        handleMove();
     }
 }
 
-void Game::switchPlayer() {
-    currentPlayer = (currentPlayer == CellState::PLAYER_X) ? CellState::PLAYER_O : CellState::PLAYER_X;
+void Game::render() {
+    board.draw();
+    gotoXY(0, 2 * BOARD_SIZE + 2);
+    setTextColor(static_cast<int>(currentPlayer->getType()));
+    cout << (currentPlayer->getType() == CellState::PLAYER_X ? "X" : "O") << "'s turn." << endl;
+    setTextColor(7);
+}
+
+void Game::handleMove() {
+    int row, col;
+    cout << "Enter row and column (0-based): ";
+    cin >> row >> col;
+
+    if (board.placePiece(row, col, currentPlayer->getType())) {
+        if (board.checkWin(row, col, currentPlayer->getType())) {
+            render();
+            displayWinner();
+            isRunning = false;
+        } else {
+            switchTurn();
+        }
+    } else {
+        cout << "Invalid move. Try again." << endl;
+    }
+}
+
+void Game::switchTurn() {
+    currentPlayer = (currentPlayer == &playerX) ? &playerO : &playerX;
 }
 
 void Game::displayWinner() {
-    // This function can be used to display the winner in a more elaborate way
-    cout << "Game Over!" << endl;
-    cout << "Player " << (currentPlayer == CellState::PLAYER_X ? "X" : "O") << " wins!" << endl;
+    gotoXY(0, 2 * BOARD_SIZE + 4);
+    setTextColor(static_cast<int>(currentPlayer->getType()));
+    cout << (currentPlayer->getType() == CellState::PLAYER_X ? "X" : "O") << " wins!" << endl;
+    setTextColor(7);
+}
+
+void Game::resetGame() {
+    board.reset();
+    currentPlayer = &playerX;
+    isRunning = true;
 }
